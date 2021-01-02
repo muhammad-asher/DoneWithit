da4ddf5a38cc5289bcd253efb6554188bd02d9fe
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-01-02 12:15:10 +0500

    Update App.js
    Initial Commit

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, Text,StyleSheet, Alert, SafeAreaView } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaView style={styles.container}>
+			<Text>Hello React </Text>
+			<Button
+				title="Click me"
+				onPress={() =>
+					Alert.alert("Title Rn", "My message", [
+						{
+							text: "Yes",
+						},
+						{
+							text: "No",
+						},
+					])
+				}
+			/>
+			<StatusBar style="auto" />
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
