e7cdfaa9c06d4c4671a89045a8ce7559b6d6335c
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-01-08 11:20:27 +0500

    Add Listing Component

--- App.js
+++ App.js
@@ -4,23 +4,8 @@ import { Text, StyleSheet, View } from "react-native";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import AppText from "./app/components/AppText";
-import AppButton from "./app/components/AppButton";
-import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-	return (
-		<View
-			style={{
-				backgroundColor: "#f8f4f4",
-				padding: 20,
-				paddingTop: 20,
-			}}
-		>
-			<Card
-				title="Red Jacket for Sale"
-				subtitle="$100"
-				image={require("./app/assets/jacket.jpg")}
-			/>
-		</View>
-	);
+	return <ListingDetailsScreen />;
 }
--- app/components/Card.js
+++ app/components/Card.js
@@ -1,22 +1,40 @@
 import React from "react";
-import { Image, View } from "react-native";
+import { Image, View, StyleSheet } from "react-native";
 import colors from "../config/colors";
 import AppText from "./AppText";
 
 function Card({ title, subtitle, image }) {
 	return (
 		<View style={styles.card}>
-			<Image source={image} />
-			<AppText>{title}</AppText>
-			<AppText>{subtitle}</AppText>
+			<Image style={styles.image} source={image} />
+			<View style={styles.detailcontainer}>
+				<AppText style={styles.title}>{title}</AppText>
+				<AppText style={styles.subtitle}>{subtitle}</AppText>
+			</View>
 		</View>
 	);
 }
 const styles = StyleSheet.create({
 	card: {
-		borderradius: 15,
+		borderRadius: 15,
 		backgroundColor: colors.white,
 		marginBottom: 20,
+        marginTop: 35,
+        overflow:"hidden"
+	},
+	image: {
+		width: "100%",
+		height: 200,
+	},
+	detailcontainer: {
+		padding: 20,
+	},
+	title: {
+		marginBottom: 7,
+	},
+	subtitle: {
+		color: colors.secondary,
+		fontWeight: "bold",
 	},
 });
 export default Card;
