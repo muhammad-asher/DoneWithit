b9499a2020abea6bb589b4804fc7e5a1c1969f46
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-01-15 17:00:08 +0500

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppText from "./app/components/AppText";
 import Icon from "./app/components/Icon";
 import Screen from "./app/components/Screen";
-import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingScreen from "./app/screens/ListingScreen";
 import AppTextInput from "./app/components/AppTextInput";
