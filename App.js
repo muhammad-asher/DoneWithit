c230b86ed138a18a0ea98ecbeb507407117f3cb1
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-01-19 17:35:11 +0500

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { Text, StyleSheet, View, TextInput } from "react-native";
 import Screen from "./app/components/Screen";
-import AccountScreen from "./app/screens/AccountScreen";
 import ListingScreen from "./app/screens/ListingScreen";
 import AppTextInput from "./app/components/AppTextInput";
 import AppPicker from "./app/components/AppPicker";
