b4e70340f120696771c38c8770ee33290298f1ea
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-02-19 18:20:26 +0500

    initial commit for add image component

--- App.js
+++ App.js
@@ -1,6 +1,25 @@
-import React from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import React, { useState,useEffect } from "react";
+import { Image } from "react-native";
+import Screen from "./app/components/Screen";
+import * as ImagePicker from "expo-image-picker";
+import { Button } from "react-native";
 
 export default function App() {
-	return <ListingEditScreen />;
+
+	const [imageUri, setImageUri] = useState();
+
+	const selectImage = async () => {
+		try {
+			const result = await ImagePicker.launchCameraAsync();
+			if (!result.cancelled) setImageUri(result.uri);
+		} catch (error) {
+			console.log("Error Reading an Image", error);
+		}
+	};
+	return (
+		<Screen>
+			<Button title="Select Image" onPress={selectImage} />
+			<Image source={{uri:imageUri}} style={{ width: 200, height: 200 }} />
+		</Screen>
+	);
 }
