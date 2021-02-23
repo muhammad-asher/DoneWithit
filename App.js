0ee5b30ac17ecad634225f5cecd7782277f254ac
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-02-23 13:34:46 +0500

    Add InputImage Functionality

--- App.js
+++ App.js
@@ -1,25 +1,19 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Image } from "react-native";
 import Screen from "./app/components/Screen";
 import * as ImagePicker from "expo-image-picker";
 import { Button } from "react-native";
+import ImageInput from "./app/components/ImageInput";
 
 export default function App() {
-
 	const [imageUri, setImageUri] = useState();
 
-	const selectImage = async () => {
-		try {
-			const result = await ImagePicker.launchCameraAsync();
-			if (!result.cancelled) setImageUri(result.uri);
-		} catch (error) {
-			console.log("Error Reading an Image", error);
-		}
-	};
 	return (
 		<Screen>
-			<Button title="Select Image" onPress={selectImage} />
-			<Image source={{uri:imageUri}} style={{ width: 200, height: 200 }} />
+			<ImageInput
+				imageUri={imageUri}
+				onChangeImage={(uri) => setImageUri(uri)}
+			/>
 		</Screen>
 	);
 }
