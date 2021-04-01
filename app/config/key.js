319bf923afd0670b74b9b5a31e6e16cb47638669
muhammad-asher <kmuhammadasher0308@gmail.com>
2021-04-01 13:30:43 +0500

    Update key.js

---
+++ app/config/key.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+	apiKey: "AIzaSyAJrdAQicnaBPVW1mzzZ4UD6F4FGxDfCaE",
+	authDomain: "donewithit-4be32.firebaseapp.com",
+	projectId: "donewithit-4be32",
+	storageBucket: "donewithit-4be32.appspot.com",
+	messagingSenderId: "346563423679",
+	appId: "1:346563423679:web:8db70e3f4d49b8c8f1c50a",
+	measurementId: "G-MFNF05ZB17",
+};
