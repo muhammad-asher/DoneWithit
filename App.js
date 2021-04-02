3e7b00f1f1a7669cd3890a5c01096e6015134244
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-04-02 17:59:35 +0500

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import navigationTheme from "./app/navigation/navigationTheme";
 import firebase from "firebase";
 import LoginScreen from "./app/screens/LoginScreen";
+import RegisterScreen from "./app/screens/RegisterScreen";
 import { firebaseConfig } from "./app/config/key";
 export default function App() {
 	const [user, setUser] = useState(null);
