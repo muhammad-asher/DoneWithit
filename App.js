1bbb28058481daebd32395e4f29cd5b01ce64d65
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-04-13 16:45:06 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,26 +1,11 @@
 import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import navigationTheme from "./app/navigation/navigationTheme";
-import firebase from "firebase";
-import LoginScreen from "./app/screens/LoginScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
-import { firebaseConfig } from "./app/config/key";
+import AppNavigator from "./app/navigation/AppNavigator";
 export default function App() {
-	const [user, setUser] = useState(null);
-
-	if (!firebase.apps.length) {
-		firebase.initializeApp(firebaseConfig);
-	}
-
-	firebase.auth().onAuthStateChanged((usr) => {
-		setUser(usr);
-	});
-
-	console.log(user);
-
 	return (
 		<NavigationContainer theme={navigationTheme}>
-			<LoginScreen />
+			<AppNavigator />
 		</NavigationContainer>
 	);
 }
