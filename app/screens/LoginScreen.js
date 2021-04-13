f48ea556884581d1332a80126af3b93ef647673e
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-04-13 16:45:02 +0500

    Update LoginScreen.js

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -2,8 +2,7 @@ import React from "react";
 import { StyleSheet, Image } from "react-native";
 import * as Yup from "yup";
 import Screen from "../components/Screen";
-import {AppForm,AppFormField,FormSubmitButton} from '../components/forms'
-
+import { AppForm, AppFormField, FormSubmitButton } from "../components/forms";
 
 const validationschema = Yup.object().shape({
 	email: Yup.string().required().email().label("Email"),
